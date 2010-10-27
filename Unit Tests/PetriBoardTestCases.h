1a8979b132163e62270f7e3855e00aa8cba5daae
Alex Rozenshteyn <rpglover64@gmail.com>
2010-10-27 18:04:57 -0400

    Add trivial test case for PetriBoard

---
+++ Unit Tests/PetriBoardTestCases.h
@@ -0,0 +1,19 @@
+//
+//  PetriBoardTestCases.h
+//  Petri
+//
+//  Created by Alexander Rozenshteyn on 10/27/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface PetriBoardTestCases : SenTestCase
+{
+
+}
+
+- (id)testCreatePetriBoard;
+
+@end
