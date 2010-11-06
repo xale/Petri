987a0fe985809f4b6bb76bec5f7921041af9c155
Alex Heinz <xale@acm.jhu.edu>
2010-11-06 17:59:36 -0400

    Added view controllers (and associated view xibs) for the three views on the main window.

---
+++ Source/Controller/View Controllers/PetriGameplayViewController.m
@@ -0,0 +1,21 @@
+//
+//  PetriGameplayViewController.m
+//  Petri
+//
+//  Created by Alex Heinz on 11/6/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriGameplayViewController.h"
+
+NSString* const PetriGameplayViewNibName =	@"GameplayView";
+
+@implementation PetriGameplayViewController
+
+- (id)init
+{
+	return [super initWithNibName:PetriGameplayViewNibName
+						   bundle:nil];
+}
+
+@end
