cf150951380ce28f390efd5acd95ac9b6d0155db
Alex Rozenshteyn <rpglover64@gmail.com>
2010-11-08 14:24:02 -0500

    Add test for creation of GameGroup

---
+++ Unit Tests/PetriGameGroupTestCases.m
@@ -0,0 +1,20 @@
+//
+//  PetriGameGroupTestCases.m
+//  Petri
+//
+//  Created by Alexander Rozenshteyn on 11/8/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriGameGroupTestCases.h"
+
+
+@implementation PetriGameGroupTestCases
+
+- (id)testCreatePetriGameGroup
+{
+	PetriGameGroup* testGameRules = [[PetriGameGroup alloc] init];
+	STAssertNotNil(testGameRules, @"PetriGameGroup object creation unsuccessful");
+}
+
+@end
