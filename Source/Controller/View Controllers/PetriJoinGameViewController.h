760975c7ab6333cd7fb9582ad0160e137506fbec
Alex Heinz <xale@acm.jhu.edu>
2010-11-10 18:40:44 -0500

    Added another view-controller and xib, for the Join-Game view.

---
+++ Source/Controller/View Controllers/PetriJoinGameViewController.h
@@ -0,0 +1,31 @@
+//
+//  PetriJoinGameViewController.h
+//  Petri
+//
+//  Created by Alex Heinz on 11/10/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriMainWindowViewController.h"
+
+/*!
+ \brief The controller for the Join Game view.
+ 
+ The PetriJoinGameViewController is responsible for the join game view, which allow the user to search for and join game groups on remote, networked machines.
+ */
+@interface PetriJoinGameViewController : PetriMainWindowViewController
+{
+	
+}
+
+/*!
+ Cancels joining a game, and returns to the Title view.
+ */
+- (IBAction)returnToTitleView:(id)sender;
+
+/*!
+ Attempts to join a game group, switching to the game group view if successful.
+ */
+- (IBAction)joinGameGroup:(id)sender;
+
+@end
