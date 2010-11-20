5a37bf6ce40bbe159e8d004c6e5ea2cb24d8b1ac
Alex Rozenshteyn <rpglover64@gmail.com>
2010-11-20 14:24:15 -0500

    Add PetriMockPlayer class for testing.

---
+++ Unit Tests/Mock Classes/PetriMockPlayer.m
@@ -0,0 +1,19 @@
+//
+//  PetriMockPlayer.m
+//  Petri
+//
+//  Created by Alexander Rozenshteyn on 11/20/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriMockPlayer.h"
+
+
+@implementation PetriMockPlayer
+
+- (id)init
+{
+	return self;
+}
+
+@end
