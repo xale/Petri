d7ca3bade64f9cea983f0a22dbedc02584683d89
Alex Rozenshteyn <rpglover64@gmail.com>
2010-11-20 18:47:14 -0500

    Add currentPiece member and nextPiece method to Game.

--- Source/Model/Game/PetriGame.h
+++ Source/Model/Game/PetriGame.h
@@ -11,6 +11,7 @@
 @class PetriPlayer;
 @class PetriGridBoard;
 @class PetriGameConfiguration;
+@class PetriPiece;
 
 @protocol PetriBoard;
 
@@ -25,6 +26,7 @@
 	PetriPlayer* currentPlayer;	/*!< The player whose turn it is. */
 	id<PetriBoard> board;			/*!< The game board. */
 	PetriGameConfiguration* gameConfiguration;	/*!< The rules for the game. */
+	PetriPiece* currentPiece; /*!< The piece available for use on the current player;s move. */
 }
 
 /*!
@@ -44,11 +46,17 @@
 - (void)addPlayersObject:(PetriPlayer*)player;
 
 /*!
+ Updates the value of current piece randomly based on the distribution provided at initialization.
+ */
+- (void)nextPiece;
+
+/*!
  Returns an immutable copy of players.
  */
 @property (readonly) NSArray* players;
 @property (readwrite, assign) PetriPlayer* currentPlayer;
 @property (readonly) id<PetriBoard>  board;
 @property (readonly) PetriGameConfiguration* gameConfiguration;
+@property (readonly) PetriPiece* currentPiece;
 
 @end
