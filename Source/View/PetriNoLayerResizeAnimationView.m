667a1dd451586ce3e06292baffba42b32f3a3a4e
Alex Heinz <xale@acm.jhu.edu>
2010-11-20 22:20:44 -0500

    [Minor] Removed the -inLiveResize checks in the resizing code for the animation-disabling NSView subclass, since these don't fire when the user clicks the 'maximize' button.

--- Source/View/PetriNoLayerResizeAnimationView.m
+++ Source/View/PetriNoLayerResizeAnimationView.m
@@ -16,22 +16,15 @@
 
 - (void)setFrame:(NSRect)frameRect
 {
-	// Check if this resize is being performed while the view is visible
-	if ([self inLiveResize])
-	{
-		// Batch CALayer resize operations into a non-animating transaction
-		[CATransaction begin];
-		[CATransaction setValue:(id)kCFBooleanTrue
-						 forKey:kCATransactionDisableActions];
-	}
+	// Batch CALayer resize operations into a non-animating transaction
+	[CATransaction begin];
+	[CATransaction setValue:(id)kCFBooleanTrue
+					 forKey:kCATransactionDisableActions];
 	
 	[super setFrame:frameRect];
 	
-	if ([self inLiveResize])
-	{
-		// End the transaction
-		[CATransaction commit];
-	}
+	// End the transaction
+	[CATransaction commit];
 }
 
 @end
