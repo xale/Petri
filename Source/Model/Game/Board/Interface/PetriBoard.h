9dbf513e43c201aeb32d65024d000ccb41840fdf
Alex Heinz <xale@acm.jhu.edu>
2010-11-22 23:28:23 -0500

    [Cosmetic] Invalidated some outdated documentation comments.

--- Source/Model/Game/Board/Interface/PetriBoard.h
+++ Source/Model/Game/Board/Interface/PetriBoard.h
@@ -16,7 +16,7 @@
 @protocol PetriBoard
 
 + (NSString*)boardType; //FIXME: document me
-+ (NSInteger)absoluteMinPlayers;	/*!< (Computed property) Specifies the minimum valid value for the minPlayers and maxPlayers properties. */
-+ (NSInteger)absoluteMaxPlayers;	/*!< (Computed property) Specifies the maximum valid value for the minPlayers and maxPlayers properties. */
++ (NSInteger)absoluteMinPlayers;	// FIXME: document me (old documentation is out-of-date: "(Computed property) Specifies the minimum valid value for the minPlayers and maxPlayers properties.")
++ (NSInteger)absoluteMaxPlayers;	// FIXME: document me (old documentation is out-of-date: "(Computed property) Specifies the maximum valid value for the minPlayers and maxPlayers properties.")
 
 @end
