e11d15549613252d5f7dcfe622b5e3aafaf287c3
Alex Heinz <xale@acm.jhu.edu>
2010-11-23 00:53:29 -0500

    Added a Mock subclass for PetriGridBoard unit testing.

---
+++ Unit Tests/Mock Classes/PetriMockGridBoard.h
@@ -0,0 +1,16 @@
+//
+//  PetriMockGridBoard.h
+//  Petri
+//
+//  Created by Alex Heinz on 11/22/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriGridBoard.h"
+
+@interface PetriMockGridBoard : PetriGridBoard
+{
+	
+}
+
+@end
---
+++ Unit Tests/Mock Classes/PetriMockGridBoard.m
@@ -0,0 +1,13 @@
+//
+//  PetriMockGridBoard.m
+//  Petri
+//
+//  Created by Alex Heinz on 11/22/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import "PetriMockGridBoard.h"
+
+@implementation PetriMockGridBoard
+
+@end
