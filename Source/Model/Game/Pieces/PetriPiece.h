138e96ea9a1a7cc717d9535e8b2e32f952b89058
Alex Rozenshteyn <rpglover64@gmail.com>
2010-11-29 12:30:01 -0500

    Add and document PetriPiece protocol

---
+++ Source/Model/Game/Pieces/PetriPiece.h
@@ -0,0 +1,29 @@
+//
+//  PetriPiece.h
+//  Petri
+//
+//  Created by Alexander Rozenshteyn on 11/29/10.
+//  Copyright 2010 Alex Heinz, Paul Martin, and Alex Rozenshteyn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@protocol PetriPiece
+
+/*!
+ Returns the current orientation of the piece; updated by calls to -rotate:
+ */
+- (NSUInteger)orientation;
+
+/*!
+ Returns the number of different orientations this piece type supports.
+ Certain pieces might have fewer unique orientations, but any piece of this type is guaranteed return to its initial orientation if rotated this many times.
+ */
++ (NSUInteger)orientationCount;
+/*!
+ Rotates a piece; updates the value of -orientation:
+ */
+- (void)rotate;
+
+@end
