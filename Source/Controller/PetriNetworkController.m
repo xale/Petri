3e98476250cf0bbc86a527ea934a1e9949f3d184
Alex Rozenshteyn <rpglover64@gmail.com>
2010-12-13 17:14:25 -0500

    Implement default networkController methods.

--- Source/Controller/PetriNetworkController.m
+++ Source/Controller/PetriNetworkController.m
@@ -7,8 +7,53 @@
 //
 
 #import "PetriNetworkController.h"
-
+#import "PetriPlayer.h"
+#import "PetriGameGroup.h"
+#import "PetriGame.h"
+#import "PetriBoard.h"
+#import "PetriBoardCell.h"
 
 @implementation PetriNetworkController
 
+- (void)updateCellId:(NSInteger)cellId
+	  withNewOwnerId:(NSInteger)ownerId
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	[[[myGame board] cellById:cellId] setOwner:[myGame playerById:ownerId]];
+}
+
+- (void)updateCellId:(NSInteger)cellId
+		 withNewItem:(PetriItem*)item
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	NSLog(@"FIXME: not implemented.");
+	//[[[myGame board] cellById:cellId] setItem:item];
+}
+
+- (void)updateCellId:(NSInteger)cellId
+	 withNewCellType:(PetriCellType)cellType
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	[[[myGame board] cellById:cellId] setCellType:cellType];
+}
+
+- (void)updatePlayerWithId:(NSInteger)playerId
+		  withAcquiredItem:(PetriItem*)item
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	[[myGame playerById:playerId] addItemsObject:item];
+}
+
+- (void)updatePlayerWithId:(NSInteger)playerId
+			  withLostItem:(PetriItem*)item
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	[[myGame playerById:playerId] removeItemsObject:item];
+}
+
+- (void)changeTurnToNewPlayerWithId:(NSInteger)playerId
+{
+	PetriGame* myGame = [[self gameGroup] game];
+	[myGame setCurrentPlayer:[myGame playerById:playerId]];
+}
 @end
